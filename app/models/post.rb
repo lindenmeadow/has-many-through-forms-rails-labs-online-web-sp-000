e44e222b5dae8d0c8bfc4e18143398d206e9483d
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-04-07 20:29:09 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,9 @@
 class Post < ActiveRecord::Base
-  has_many :post_categories
-  has_many :categories, through: :post_categories
+  
   has_many :comments
   has_many :users, through: :comments
 
-  accepts_nested_attributes_for :categories
+  accepts_nested_attributes_for :categories, :comments
 
   def unique_commentors
     self.comments.collect {|c| c.user}.uniq
