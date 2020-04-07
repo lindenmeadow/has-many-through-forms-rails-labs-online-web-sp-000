5a34311c777875c58da23aa379993bf0c566e91f
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-04-07 20:42:47 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,12 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
-  accepts_nested_attributes_for :categories, :comments
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      category = Category.find_or_create_by(category_attribute)
+      self.categories << category
+    end
+  end
 
   def unique_commentors
     self.comments.collect {|c| c.user}.uniq
